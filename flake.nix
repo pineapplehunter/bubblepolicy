0e8659025b417d325236983fbdfc4444f3c5360a
Shogo Takata <shogo-takata@st.go.tuat.ac.jp>
2026-03-20 01:18:18 +0900

    create nix package

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A basic shell";
+  description = "bubblepolicy flake";
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   inputs.flake-parts.url = "github:hercules-ci/flake-parts";
@@ -10,53 +10,47 @@
 
   outputs =
     { flake-parts, ... }@inputs:
-    flake-parts.lib.mkFlake { inherit inputs; } (
-      { config, ... }:
-      {
-        # Custom overlays for this shell
-        flake.overlays.default = final: prev: { };
+    flake-parts.lib.mkFlake { inherit inputs; } {
 
-        systems = [
-          "aarch64-darwin"
-          "aarch64-linux"
-          "x86_64-darwin"
-          "x86_64-linux"
-        ];
+      systems = [
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
 
-        perSystem =
-          { pkgs, system, ... }:
-          {
-            _module.args.pkgs = import inputs.nixpkgs {
-              inherit system;
-              overlays = [
-                inputs.rust-overlay.overlays.default
-                config.flake.overlays.default
-                # Add overlays as needed
-              ];
-            };
-
-            devShells.default = pkgs.mkShell {
-              packages = with pkgs; [
-                pkg-config
-                rustPlatform.bindgenHook
-                strace
-                bubblewrap
-                (rust-bin.stable.latest.default.override {
-                  extensions = [
-                    "rust-src"
-                    "rust-analyzer"
-                  ];
-                  targets = [ ];
-                })
-              ];
-            };
-
-            # Use nixfmt for all nix files
-            formatter = pkgs.nixfmt-tree;
-
-            # Uncomment to build any package with `nix build .#package`
-            #legacyPackages = pkgs;
+      perSystem =
+        { pkgs, system, ... }:
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              inputs.rust-overlay.overlays.default
+            ];
           };
-      }
-    );
+
+          packages = rec {
+            default = bubblepolicy;
+            bubblepolicy = pkgs.callPackage ./package.nix { };
+          };
+
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              pkg-config
+              rustPlatform.bindgenHook
+              strace
+              bubblewrap
+              # Rust toolchain with rust-src (for procedural macros) and rust-analyzer
+              (rust-bin.stable.latest.default.override {
+                extensions = [
+                  "rust-src"
+                  "rust-analyzer"
+                ];
+                targets = [ ];
+              })
+            ];
+          };
+
+          # Use nixfmt for all nix files
+          formatter = pkgs.nixfmt-tree;
+        };
+    };
 }
---
+++ package.nix
@@ -0,0 +1,29 @@
+{
+  rustPlatform,
+  lib,
+  makeWrapper,
+  strace,
+}:
+
+rustPlatform.buildRustPackage {
+  pname = "bubblepolicy-unwrapped";
+  version = "0.1.0";
+
+  src = lib.fileset.toSource {
+    root = ./.;
+    fileset = lib.fileset.unions [
+      ./Cargo.lock
+      ./Cargo.toml
+      ./src
+    ];
+  };
+
+  cargoLock.lockFile = ./Cargo.lock;
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  postInstall = ''
+    wrapProgram $out/bin/bubblepolicy \
+      --suffix PATH : "${lib.makeBinPath [ strace ]}"
+  '';
+}
